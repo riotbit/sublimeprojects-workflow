d780faceb3b53dd363969c6e7bcef3cf663e065b
Christian Klopp <christian.klopp@riotbit.de>
2016-05-25 00:04:53 +0200

    pep8

--- sublimeprojects-workflow.py
+++ sublimeprojects-workflow.py
@@ -2,10 +2,9 @@
 # encoding: utf-8
 
 import sys
-
 from workflow import Workflow
-
 log = None
+
 
 def main(wf):
     import subprocess
@@ -14,23 +13,31 @@ def main(wf):
     args = wf.args
 
     # get the paths of query matches
-    project_paths = subprocess.check_output(['mdfind', 'kMDItemFSName=*.sublime-project']).splitlines()
+    project_paths = subprocess.check_output([
+                                            'mdfind',
+                                            'kMDItemFSName=*.sublime-project'
+                                            ]).splitlines()
     # decode project_paths to normalised unicode
-    for i in range(0,len(project_paths)-1):
+    for i in range(0, len(project_paths) - 1):
         project_paths[i] = wf.decode(project_paths[i])
 
     if not args:
-        query_matches = project_paths # return all project files
+        query_matches = project_paths  # return all project files
     else:
         query_matches = wf.filter(args[0], project_paths)
 
     # Add matches to Alfred feedback
     for match_path in query_matches:
-        wf.add_item(match_path.split('/')[-1].split('.')[0], match_path, uid=match_path, arg=match_path, valid=True, icon="icon.png")
+        file_name = match_path.split('/')[-1].split('.')[0]
+        wf.add_item(file_name,
+                    match_path,
+                    uid=match_path,
+                    arg=match_path,
+                    valid=True,
+                    icon="icon.png")
 
     # Send output to Alfred
     wf.send_feedback()
-
 
 if __name__ == '__main__':
     wf = Workflow()
